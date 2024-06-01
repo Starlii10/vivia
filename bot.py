3e47ae84961980c9d05dabbbdf1cb4bba5e99c6d
WiseDragoon5374 <lukeilderton@outlook.com>
2024-06-01 16:10:48 -0400

    remove built in help command to make our own

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ import random
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+bot.remove_command("help")
 
 helpMsg = open("helpmsg.txt", "r").read()
 
