d4d7c504d2aa5f477f2f8ca345f7e7b1dda42d9b
WiseDragoon5374 <lukeilderton@outlook.com>
2024-09-19 17:00:34 -0400

    Update command template with more info, add more messages, why not

--- extras/viviatools.py
+++ extras/viviatools.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-    This script contains functions for Vivia and her extensions.
+    This is ViviaTools, a helper script for Vivia that contains commonly used functions.
 
     Vivia is licensed under the MIT License. For more information, see the LICENSE file.
     TL:DR: you can use Vivia's code as long as you keep the original license intact.
