3c3fa4fc2cc4cda3ba3a4af80a6f8edd5db9eac0
WiseDragoon5374 <lukeilderton@outlook.com>
2024-09-27 21:28:50 -0400

    bro

--- extras/viviatools.py
+++ extras/viviatools.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 loaded_extensions = set(str())
 failed_extensions = set(str())
 running = False
-bot_ref = commands.Bot("")
+bot_ref = commands.Bot("", intents=discord.Intents.default())
 
 # Config loading
 config = configparser.ConfigParser()
