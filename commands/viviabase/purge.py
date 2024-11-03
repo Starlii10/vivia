bb949e3d32bc5a2291217ae85ebb3bda41cec488
WiseDragoon5374 <lukeilderton@outlook.com>
2024-11-03 08:45:25 -0500

    redo purge command

--- commands/viviabase/purge.py
+++ commands/viviabase/purge.py
@@ -11,29 +11,53 @@
 """
 
 from discord.ext import commands
-from discord import app_commands
+from discord import Message, app_commands
 from extras.viviatools import personalityMessage, adminOnly
 
 async def setup(bot: commands.Bot): # for extension loading
     bot.add_command(purge)
 
-@commands.hybrid_command()
+@commands.hybrid_command(
+    name="purge",
+    description="Purges (deletes) messages between start and end."
+)
 @app_commands.describe(
     start="Message ID or link to start of range",
     end="Message ID or link to end of range"
 )
 @adminOnly
-async def purge(ctx: commands.Context, start: int | str = None, end: int | str = None):
+async def purge(ctx: commands.Context, start: int = None, end: int = None):
     """
-        Purges messages between start and end.
+        Purges (deletes) messages between start and end.
+
+        # Args:
+            - start (int, optional): The ID of the message to start purging from. Defaults to None.
+            - end (int, optional): The ID of the message to end purging at. Defaults to None.
+
+        # Notes:
+            - If both start and end are None, all messages in the channel are purged.
+            - If start is None, all messages up to the end are purged.
+            - If end is None, all messages starting from the start are purged.
     """
-    if start is None and end is None:
-        await ctx.channel.purge(limit=None)
+
+    if start == None and end == None:
+        await ctx.channel.purge()
     else:
-        if start is None:
-            await ctx.channel.purge(limit=end)
-        elif end is None:
-            await ctx.channel.purge(limit=start)
-        else:
-            await ctx.channel.purge(limit=end - start + 1)
+        # IDs to objects
+        if start != None:
+            start = await ctx.channel.fetch_message(start)
+        if end != None:
+            end = await ctx.channel.fetch_message(end)
+
+        # Convert to timestamps
+        if start != None:
+            start = start.created_at
+        if end != None:
+            end = end.created_at
+            
+        # Purge
+        await ctx.channel.purge(
+            after=start,
+            before=end
+        )
     await ctx.send(personalityMessage("purge"))
